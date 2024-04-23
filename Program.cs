753f6e1aff8af4baba9488d917092b118bc15ea2
makif34 <sanlanakif32@gmail.com>
2024-04-23 18:44:58 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kampİntro1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("azalış butonu");
+
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else { Console.WriteLine("Değişmedi Buttonu"); }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
